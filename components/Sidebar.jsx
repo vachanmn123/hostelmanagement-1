a2a65d750bdd89aa5d3a49ea34ae806e40a8523c
Vachan MN <vachanmn123@gmail.com>
2024-01-11 11:32:26 +0530

    Add Sidebar component with Accordion and Button

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,29 +4,28 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function Sidebar() {
   return (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It's animated by default, but you can disable it if you prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+    <div className="py-5 px-5">
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Block 1</AccordionTrigger>
+          <AccordionContent>
+            <Button className="w-full my-2">Floor 1</Button>
+            <Button className="w-full my-2">Floor 2</Button>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Block 2</AccordionTrigger>
+          <AccordionContent>
+            <Button className="w-full my-2">Floor 1</Button>
+            <Button className="w-full my-2">Floor 2</Button>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
   );
 }
