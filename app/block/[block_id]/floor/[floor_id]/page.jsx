06bec21ed019765ce0ff03f10f81a120da3dd078
1amSumit <sj779619@gmail.com>
2024-01-11 06:52:37 +0530

    dd

--- app/block/[block_id]/floor/[floor_id]/page.jsx
+++ app/block/[block_id]/floor/[floor_id]/page.jsx
@@ -1,7 +1,7 @@
 import RoomCompo from "@/components/RoomCompo";
 import React from "react";
 
-export default function page({ params }) {
+export default function page() {
   return (
     <div className="pt-6 px-4 grid grid-cols-2 gap-2">
       <RoomCompo />
--- components/RoomCompo.jsx
+++ components/RoomCompo.jsx
@@ -1,48 +1,39 @@
 "use client";
 
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 import { FaBath } from "react-icons/fa6";
+import Link from "next/link";
 
 export default function RoomCompo() {
   return (
-    <Card className=" shadow-xl">
+    <Card className="shadow-xl">
       <CardContent className="pt-2">
         <p>Room ID : 1</p>
       </CardContent>
       <CardContent className="flex gap-10 justify-center">
-        <figure className="w-[100px] h-[100px] border-2 border-slate-300 flex items-center justify-center rounded-full">
-          <img
-            src="/img.avif"
-            alt="profile-image"
-            className="rounded-full w-[100
+        <Link href={"/student/1"}>
+          <figure className="w-[100px] h-[100px] border-2 border-slate-300 flex items-center justify-center rounded-full">
+            <img
+              src="/img.avif"
+              alt="profile-image"
+              className="rounded-full w-[100
         %] h-[100%]"
-          />
-        </figure>
-        <figure className="w-[100px] h-[100px] border-2 border-slate-300 flex items-center justify-center rounded-full">
-          <img
-            src="/img.avif"
-            alt="profile-image"
-            className="rounded-full w-[100
+            />
+          </figure>
+        </Link>
+        <Link href={"/student/1"}>
+          <figure className="w-[100px] h-[100px] border-2 border-slate-300 flex items-center justify-center rounded-full">
+            <img
+              src="/img.avif"
+              alt="profile-image"
+              className="rounded-full w-[100
                 %] h-[100%]"
-          />
-        </figure>
-        <figure className="w-[100px] h-[100px] border-2 border-slate-300 flex items-center justify-center rounded-full bg-gray-300">
-          {/* <img
-                    src="/img.avif"
-                    alt="profile-image"
-                    className="rounded-full w-[100
-                %] h-[100%]"
-                  /> */}
-        </figure>
+            />
+          </figure>
+        </Link>
+        <figure className="w-[100px] h-[100px] border-2 border-slate-300 flex items-center justify-center rounded-full bg-gray-300"></figure>
       </CardContent>
       <CardContent>
         <p>
