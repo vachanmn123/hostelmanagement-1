2d59a9248c403ac7054b13c166a9ba3a38d00d80
Vachan MN <vachanmn123@gmail.com>
2024-01-11 11:21:10 +0530

    Add @radix-ui/react-accordion and Sidebar components

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <Sidebar />
         {children}
       </body>
     </html>
