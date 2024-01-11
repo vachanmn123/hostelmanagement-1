a2a6916cbe22a0ebe615aaf5511afa7a980e1969
Vachan MN <vachanmn123@gmail.com>
2024-01-11 11:19:01 +0530

    Add Navbar component to RootLayout

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-center">
+      <div>
+        <h1 className="text-xl upppercase ">Hostel Management</h1>
+      </div>
+    </div>
+  );
+}
